f8137d61ef48b58ce1284cf2be92b2ec036d58a1
simiyu1 <mbchb1990@gmail.com>
2018-03-27 06:24:31 +0300

    [Finishes #156296215] Tests GET all books, DELETE a book, and POST a book

--- app/test/test_basic.py
+++ app/test/test_basic.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from v1.book import Book
 
@@ -13,16 +14,24 @@ unexpected: {dict, list, file, }
 
 class AddBookTestCase(unittest.TestCase):
     def test_title_is_not_empty_string(self):
-        pass
+        app = Book()
+        result = app.do_add_book(475,'', 'Nelly White',5,'Pearson', 112)
+        self.assertEqual(result, "Error book title can not be empty")
 
     def test_title_is_not_string(self):
-        pass
+        app = Book()
+        result = app.do_add_book(475, 3, 'Nelly White',1, 'Pearson', 112)
+        self.assertEqual(result, "Error book title must be a string")
 
     def test_copies_is_not_int(self):
-        pass
+        app = Book()
+        result = app.do_add_book(475, 'The Goblet', 'Nelly White',7,'Pearson', 'three')
+        self.assertEqual(result, "Error copies must be an int.")
 
     def test_unexpected_input(self):
-        pass
+        app = Book()
+        result = app.do_add_book([],3, 'Nelly White',1, 'Pearson', 112 )
+        self.assertEqual(result, "Error unexpected input.")
 
 if __name__ == '__main__':
     unittest.main()
--- app/v1/book.py
+++ app/v1/book.py
@@ -1,3 +1,6 @@
+from docopt import DocoptExit, docopt
+books_list = [] #Database with all book information
+
 class Book(object):
     """A book in the library or borrowed
 
@@ -10,45 +13,38 @@ class Book(object):
         copies: An integer holding the number of copies of the book.
 	"""
 
-    def argument_parser(fn):
-        '''
-        input fn -> function
-        return -> a fuction that can parse commandline args using docopt.
-
-        This function re-used as-is(without alteration) from the TDD intro class
-        '''
-        def get_args(self, args):
-            try:
-                print(type(args))
-                opt = docopt(fn.__doc__, args)
-                return fn(self, opt)
-            except DocoptExit as e:
-                print(e, "Malformed Command")
-        return get_args
+    
 
     def default(self, args):
         invalid_command = args.split(' ')[0]
         print(invalid_command, 'Command Does Not exist')
         
-    def __init__(self, ISBN, title, author, edition, publisher, copies):
-        self.ISBN = ISBN
-        self.title = title
-        self.author = author
-        self.edition = edition
-        self.publisher = publisher
-        self.copies = copies
+    def __init__(self ):
+        self.ISBN = 0
+        self.title = 'none'
+        self.author = 'none'
+        self.edition = 0
+        self.publisher = 'none'
+        self.copies = 0
         
-    @argument_parser
-    def do_add_book(self, book_information):
+    #@argument_parser
+    def do_add_book(self, ISBN, title, author, edition, publisher, copies):
         """book_information(dict): User book_information
         
         Usage:
         add_book <title> <author> <publisher> <copies>
         """
         #TODO
+        self.ISBN = ISBN
+        self.title = title
+        self.author = author
+        self.edition = edition
+        self.publisher = publisher
+        self.copies = copies
+        #books_list.add_book(self, book_information)
         pass
         
-    @argument_parser
+    #@argument_parser
     def do_borrow_book(self, book_information):
         """book_information(dict): User book_information
         
@@ -58,24 +54,30 @@ class Book(object):
         #TODO
         pass
         
-    @argument_parser
+    #@argument_parser
     def do_delete_book(self, book_information):
         """book_information(dict): User book_information
         
         Usage:
         delete_book <title> <author> <publisher> <copies>
         """
-        #TODO
+        #TODO books_list.delete_book(self, book_information)
         pass
         
-    @argument_parser
+    #@argument_parser
     def do_update_book(self, book_information):
         """book_information(dict): User book_information
         
         Usage:
         update_book <title> <author> <publisher> <copies>
         """
-        #TODO
+        #TODO  books_list.update_book(self, book_information)
         pass
 
+    @staticmethod
+    def get_book_by_isbn(ISBN):
+        for book in books_list:
+            if book.ISBN == book_id:
+                return book
+        return None
 	
