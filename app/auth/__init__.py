b626d243469a80c887c274ab2b12a6250691ac96
simiyu1 <mbchb1990@gmail.com>
2018-04-01 21:17:06 +0300

    [Finishes  #156422805] Travis web tokens added

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 auth = Blueprint('authentication', __name__, url_prefix = '/api/v1/auth/')
 
-from app.auth.views import Register,Reset 
+from app.auth.views import Register,Reset, Login 
 from app.userdir import user
 
 from app import app
@@ -11,4 +11,5 @@ from flask_restful import Api
 api = Api(app)
 
 api.add_resource(Register, '/api/v1/auth/register')
+api.add_resource(Login, '/api/v1/auth/login')
 api.add_resource(Reset, '/api/v1/auth/reset')
