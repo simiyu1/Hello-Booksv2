0aa314f66d63ea85bd5505f454714baff30790e4
simiyu1 <mbchb1990@gmail.com>
2018-04-03 12:57:47 +0300

    ch: Attempt four to fix gunicorn

--- run.py
+++ run.py
@@ -3,17 +3,14 @@ from app.bookdir import book
 from app.userdir import user
 from app.auth import auth
 
-# from flask import Flask
-import app
-app = app.app
+from flask import Flask
+from app import app
+
 
 # Register the blueprints
 app.register_blueprint(book)
 app.register_blueprint(user)
 app.register_blueprint(auth)
-@app.route("/", methods =['GET'])
-def home():
-    return "Hello world"
 #adding the config
 
 if __name__ == '__main__':
