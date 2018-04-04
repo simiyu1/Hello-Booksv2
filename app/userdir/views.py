28cc40a16dd55d573b85cdf3e5174c6667fbaae6
simiyu1 <mbchb1990@gmail.com>
2018-04-04 11:11:02 +0300

    [Finishes #156453135] Use kebab case consistently

--- app/userdir/views.py
+++ app/userdir/views.py
@@ -25,9 +25,9 @@ borrowed_books.append(book2)
 class Users(Resource):
     def get(self, userid=None):
         if request.args.get('userid') != None:
-            senID = request.args.get('userid')
+            received_ID = request.args.get('userid')
             items = []
-            items = [user for user in users_list if user.userid == int(senID)]
+            items = [user for user in users_list if user.userid == int(received_ID)]
             if len(items) < 1:
                 return 'User not found', 404
             return ({'user':{'userid': items[0].userid, 'username': items[0].username, 'role': items[0].role}}, {'message': 'Fetched User'}), 200
@@ -42,14 +42,14 @@ class Users(Resource):
 
 class Borrow(Resource):
     def post(self, ISBN=None):
-        sentISBN = request.args.get('ISBN')
+        recieved_ISBN = request.args.get('ISBN')
         borrowed = []
-        if sentISBN == None:
+        if recieved_ISBN == None:
             return 'You Have to specify the book ISBN',200
         if len(books_list) < 1: #Chec if there are books in the library
             return {"Message":"No Books in the library"}, 404
         else:
-            item_id = int(sentISBN)
+            item_id = int(received_ID)
             book = [item for item in books_list if item.ISBN == item_id ]
             if not book:
                 return {"Message": "No book found with the given book id"}, 404
